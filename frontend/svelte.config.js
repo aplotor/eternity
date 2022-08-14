5a675308835b728fa3eb46dac08d9ae5a793c683
jc9108 <jc@portals.sh>
2022-08-13 21:08:21 -0400

    .

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,7 +5,9 @@ export default { // https://kit.svelte.dev/docs/configuration
 		".svelte"
 	],
 	kit: {
-		adapter: adapter_static(), // an adapter is required to build for prod. see https://kit.svelte.dev/docs/adapters
+		adapter: adapter_static({
+			fallback: true
+		}), // an adapter is required to build for prod. see https://kit.svelte.dev/docs/adapters
 		files: {
 			template: "./source/app.html",
 			routes: "./source/routes/",
