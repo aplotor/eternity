d10d99f643d9753602b0adbf8d1ee23a0e705614
jc9108 <jc@portals.sh>
2022-10-15 12:56:28 -0400

    update build commands

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 if [ "$1" = "dev" ]; then
 	if [ "$2" = "build" ]; then
 		(cd ./backend/ && npm install)
-		(cd ./frontend/ && npm install)
-		cd ./frontend/ && npm run build
+		cd ./frontend/ && npm install && npm run build
 		return
 	elif [ "$2" = "up" ]; then
 		concurrently --names "backend,frontend" --prefix "{name}:" --prefix-colors "#f2caff,#61f2f2" "cd ./backend/ && npm run dev" "wait-for-it 0.0.0.0:1301 -t 0 && cd ./frontend/ && npm run dev"
@@ -13,8 +12,7 @@ if [ "$1" = "dev" ]; then
 elif [ "$1" = "prod" ]; then
 	if [ "$2" = "build" ]; then
 		(cd ./backend/ && npm ci)
-		(cd ./frontend/ && npm ci)
-		cd ./frontend/ && npm run build
+		cd ./frontend/ && npm ci && npm run build
 		return
 	elif [ "$2" = "up" ]; then
 		cd ./backend/ && npm run prod_start
